688c528fc0e1f4c23e232bed90f6509fb27fec7d
Tity.S <tity.septiani@gmail.com>
2022-05-04 23:32:13 +0700

    Create helper function for creating the instance of the SUT

--- EDGameEngineTests/FlowTests.swift
+++ EDGameEngineTests/FlowTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import EDGameEngine
 
 class FlowTests:XCTestCase {
+    let router = RouterSpy()
+    
     // Think of the most basic thing, like when the game starts and there is no questions available
     // How it would behave
     // In this case the flow would go straight to the result presentation
@@ -17,9 +19,7 @@ class FlowTests:XCTestCase {
     // in this case, the router will be responsible for routing the result
     // In this stage, we also don't know what the result would look like
     func test_start_withNoQuestions_doesNotRouteToQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions:[], router:router)
-        sut.start()
+        makeSUT(questions: []).start()
         XCTAssertTrue(router.routedQuestions.isEmpty)
     }
     
@@ -33,17 +33,13 @@ class FlowTests:XCTestCase {
 //
     // After the routing is successful on the 2nd test, we need to check if the routing gives the correct answer
     func test_start_oneQuestion_routesToCorrectQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions:["Q1"], router:router)
-        sut.start()
+        makeSUT(questions: ["Q1"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
     // After the first correct question routed, we need to check if a different question will still give the correct result
     func test_start_oneQuestion_routesToCorrectQuestion_2() {
-        let router = RouterSpy()
-        let sut = Flow(questions:["Q2"], router:router)
-        sut.start()
+        makeSUT(questions: ["Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q2"])
     }
     
@@ -51,16 +47,13 @@ class FlowTests:XCTestCase {
     // After we successfully test the router results for only one question,
     // now we need to test with two questions and see if it will route correctly to the first question
     func test_start_twoQuestions_routesToFirstQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions:["Q1", "Q2"], router:router)
-        sut.start()
+        makeSUT(questions: ["Q1", "Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
     // If the router is being started twice, the question routed should also be the same twice
     func test_startTwice_twoQuestions_routesToFirstQuestionTwice() {
-        let router = RouterSpy()
-        let sut = Flow(questions:["Q1", "Q2"], router:router)
+        let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
         sut.start()
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q1"])
@@ -70,11 +63,18 @@ class FlowTests:XCTestCase {
     // Because of this, we need a way to capture the answer behavior inside the spy
     // Thus, we're adding answer callback closure on the router protocol
     func test_startAndAnswerFirstQuestion_withTwoQuestions_routesToSecondQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions:["Q1", "Q2"], router:router)
+        let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
         router.answerCallback("A1")
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
+    }
+    
+    
+    // MARK: - Helper
+    
+    // By doing this, we can change the initializer without breaking the tests
+    func makeSUT(questions:[String]) -> Flow {
+        return Flow(questions:questions, router:router)
     }
     
     // Spy is the object and you just spying or stubbing specific methods of it.
