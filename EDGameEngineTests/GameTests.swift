9dad0076099713d32b42a265fc545b2963b5a2b1
Tity.S <tity.septiani@gmail.com>
2022-05-22 23:32:33 +0700

    - Make the test test_startGame_answerOneOutOfTwoCorrectly_scores1() failing and not crashing anymore by adding game variable

--- EDGameEngineTests/GameTests.swift
+++ EDGameEngineTests/GameTests.swift
@@ -14,7 +14,7 @@ class GameTests:XCTestCase {
     // we simulate the startGame function and also simulate the answer with router answercallback
     func test_startGame_answerOneOutOfTwoCorrectly_scores1() {
         let router = RouterSpy()
-        startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1":"A1", "Q2":"A2"])
+        let _ = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1":"A1", "Q2":"A2"])
         router.answerCallback("A1")
         router.answerCallback("wrong answer")
         XCTAssertEqual(router.routedResult!.score, 1)
