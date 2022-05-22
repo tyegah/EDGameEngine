7c16acea51d3770a91401884debe1477b4e2fa8b
Tity.S <tity.septiani@gmail.com>
2022-05-22 22:42:57 +0700

    - Create helper function to create result on Flow - Update result variable name with answers to avoid name conflict with the result() method

--- EDGameEngine/Flow.swift
+++ EDGameEngine/Flow.swift
@@ -22,7 +22,7 @@ class Flow <Question, Answer, R:Router> where R.Question == Question, R.Answer =
 {
     private let router:R
     private let questions:[Question]
-    private var result:[Question:Answer] = [:]
+    private var answers:[Question:Answer] = [:]
     init(questions: [Question], router:R) {
         self.router = router
         self.questions = questions
@@ -33,7 +33,7 @@ class Flow <Question, Answer, R:Router> where R.Question == Question, R.Answer =
             router.routeTo(question: firstQuestion, answerCallback: nextCallback(from: firstQuestion))
         }
         else {
-            router.routeTo(result: Result(answers: result))
+            router.routeTo(result: result())
         }
     }
     
@@ -47,16 +47,20 @@ class Flow <Question, Answer, R:Router> where R.Question == Question, R.Answer =
     
     private func routeNext(_ question:Question,_ answer:Answer) {
         if let currentQuestionIndex = questions.firstIndex(of: question) {
-            result[question] = answer
+            answers[question] = answer
             let nextQuestionIndex = currentQuestionIndex+1
             if nextQuestionIndex < questions.count {
                 let nextQuestion = questions[nextQuestionIndex]
                 router.routeTo(question: nextQuestion, answerCallback: nextCallback(from: nextQuestion))
             }
             else {
-                router.routeTo(result: Result(answers: result))
+                router.routeTo(result: result())
             }
         }
+    }
+    
+    private func result() -> Result<Question,Answer> {
+        return  Result(answers: answers)
     }
 }
 
