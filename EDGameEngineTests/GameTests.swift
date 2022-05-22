fb999f6d6fd746436ff444659e6ab933d70285c3
Tity.S <tity.septiani@gmail.com>
2022-05-22 23:36:50 +0700

    - Move the Game And RouterSpy objects outside the test function to hold strong references to them

--- EDGameEngineTests/GameTests.swift
+++ EDGameEngineTests/GameTests.swift
@@ -9,6 +9,9 @@ import XCTest
 import EDGameEngine // We dont need @testable because we want to make it public for the integration test
 
 class GameTests:XCTestCase {
+    // We move the game and router objects here to make sure they hold strong references in the test
+    var router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
     // Here we want to start testing if the game scoring function is running correctly,
     // and we start out by calculating two questions with one answer correct
     // we simulate the startGame function and also simulate the answer with router answercallback
